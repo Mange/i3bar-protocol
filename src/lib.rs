518df8a4af8058beb2133c3513174751f56b4877
Magnus Bergmark <magnus.bergmark@gmail.com>
2017-08-07 17:04:24 +0200

    Add ClickEventBuilder; make optional fields optional

--- src/lib.rs
+++ src/lib.rs
@@ -51,10 +51,10 @@ impl From<serde_json::Error> for ParseError {
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct ClickEvent {
     name: String,
-    instance: String,
+    instance: Option<String>,
     button: MouseButton,
-    x: u32,
-    y: u32,
+    x: Option<u32>,
+    y: Option<u32>,
 }
 
 impl ClickEvent {
@@ -66,24 +66,75 @@ impl ClickEvent {
         &self.name
     }
 
-    pub fn instance(&self) -> &str {
-        &self.instance
+    pub fn instance(&self) -> Option<&str> {
+        self.instance.as_ref().map(String::as_ref)
     }
 
     pub fn button(&self) -> MouseButton {
         self.button
     }
 
-    pub fn coordinates(&self) -> (u32, u32) {
-        (self.x, self.y)
+    pub fn coordinates(&self) -> Option<(u32, u32)> {
+        if let (Some(x), Some(y)) = (self.x, self.y) {
+            Some((x, y))
+        } else {
+            None
+        }
     }
 
-    pub fn x(&self) -> u32 {
+    pub fn x(&self) -> Option<u32> {
         self.x
     }
 
-    pub fn y(&self) -> u32 {
+    pub fn y(&self) -> Option<u32> {
         self.y
+    }
+}
+
+pub struct ClickEventBuilder {
+    name: String,
+    button: MouseButton,
+
+    instance: Option<String>,
+    x: Option<u32>,
+    y: Option<u32>,
+}
+
+impl ClickEventBuilder {
+    pub fn new<S>(name: S, button: MouseButton) -> ClickEventBuilder
+        where S: Into<String>
+    {
+        ClickEventBuilder {
+            name: name.into(),
+            button: button,
+            instance: None,
+            x: None,
+            y: None,
+        }
+    }
+
+    pub fn instance<V, S>(mut self, value: V) -> Self
+        where V: Into<Option<S>>,
+              S: Into<String>
+    {
+        self.instance = value.into().map(Into::into);
+        self
+    }
+
+    pub fn coordinates(mut self, x: u32, y: u32) -> Self {
+        self.x = Some(x);
+        self.y = Some(y);
+        self
+    }
+
+    pub fn build(self) -> ClickEvent {
+        ClickEvent {
+            name: self.name,
+            instance: self.instance,
+            button: self.button,
+            x: self.x,
+            y: self.y,
+        }
     }
 }
 
@@ -99,14 +150,32 @@ mod tests {
     use super::*;
 
     #[test]
+    fn it_allows_building_of_click_events() {
+        let small_event = ClickEventBuilder::new("name", MouseButton::Right).build();
+        assert_eq!(small_event.name(), "name");
+        assert_eq!(small_event.instance(), None);
+        assert_eq!(small_event.button(), MouseButton::Right);
+        assert_eq!(small_event.coordinates(), None);
+
+        let big_event = ClickEventBuilder::new("name", MouseButton::Right)
+            .instance("instance")
+            .coordinates(100, 200)
+            .build();
+        assert_eq!(big_event.name(), "name");
+        assert_eq!(big_event.instance(), Some("instance"));
+        assert_eq!(big_event.button(), MouseButton::Right);
+        assert_eq!(big_event.coordinates(), Some((100, 200)));
+    }
+
+    #[test]
     fn it_serdes_mouse_event() {
         let event_string = r#"{"name":"ethernet","instance":"eth0","button":1,"x":1320,"y":1400}"#;
         let event = ClickEvent::from_str(event_string).expect("Failed to parse");
 
         assert_eq!(event.name(), "ethernet");
-        assert_eq!(event.instance(), "eth0");
+        assert_eq!(event.instance(), Some("eth0"));
         assert_eq!(event.button(), MouseButton::Left);
-        assert_eq!(event.coordinates(), (1320, 1400));
+        assert_eq!(event.coordinates(), Some((1320, 1400)));
 
         assert_eq!(event.to_string(), String::from(event_string));
     }
